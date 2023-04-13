fd4077d6439831d198d88d29fc85db90a7cdbdf6
gopher <gopher.hu@gmail.com>
2023-04-13 11:19:24 +0200

    Adapt to latest Micro

--- admin/src/AdminApp.php
+++ admin/src/AdminApp.php
@@ -2,33 +2,40 @@
 
 namespace Dynart\Press\Admin;
 
-use Dynart\Micro\AnnotationProcessor;
 use Dynart\Micro\Database;
-use Dynart\Micro\LocaleResolver;
-use Dynart\Micro\RouteAnnotation;
+use Dynart\Micro\Database\PdoBuilder;
+use Dynart\Micro\Database\MariaDatabase;
+use Dynart\Micro\Middleware\LocaleResolver;
+use Dynart\Micro\Middleware\AnnotationProcessor;
+use Dynart\Micro\Annotation\RouteAnnotation;
 use Dynart\Micro\Translation;
 use Dynart\Micro\View;
 use Dynart\Micro\WebApp;
 
 class AdminApp extends WebApp {
+
     public function __construct(array $configPaths) {
         parent::__construct($configPaths);
 
-        $this->add(Database::class);
+        $this->add(PdoBuilder::class);
+        $this->add(Database::class, MariaDatabase::class);
         $this->add(Translation::class);
         $this->add(RouteAnnotation::class);
         $this->addMiddleware(LocaleResolver::class);
         $this->addMiddleware(AnnotationProcessor::class);
 
         $this->add(Controller\DashboardController::class);
-    }
 
-    public function init() {
         $annotations = $this->get(AnnotationProcessor::class);
         $annotations->addNamespace('Dynart\\Press\\Admin\\Controller');
         $annotations->add(RouteAnnotation::class);
+    }
 
+    public function init() {
         parent::init();
+
+        $translation = $this->get(Translation::class);
+        $translation->add('admin', '~/admin/translations');
 
         $view = $this->get(View::class);
         $view->addFolder('admin', '~/admin/views');
--- src/PressApp.php
+++ src/PressApp.php
@@ -2,11 +2,13 @@
 
 namespace Dynart\Press;
 
-use Dynart\Micro\LocaleResolver;
-use Dynart\Micro\AnnotationProcessor;
-use Dynart\Micro\RouteAnnotation;
-use Dynart\Micro\Translation;
 use Dynart\Micro\Database;
+use Dynart\Micro\Database\PdoBuilder;
+use Dynart\Micro\Database\MariaDatabase;
+use Dynart\Micro\Middleware\LocaleResolver;
+use Dynart\Micro\Middleware\AnnotationProcessor;
+use Dynart\Micro\Annotation\RouteAnnotation;
+use Dynart\Micro\Translation;
 use Dynart\Micro\WebApp;
 
 class PressApp extends WebApp {
@@ -14,7 +16,8 @@ class PressApp extends WebApp {
     public function __construct(array $configPaths) {
         parent::__construct($configPaths);
 
-        $this->add(Database::class);
+        $this->add(PdoBuilder::class);
+        $this->add(Database::class, MariaDatabase::class);
         $this->add(Translation::class);
         $this->add(RouteAnnotation::class);
         $this->addMiddleware(LocaleResolver::class);
@@ -23,13 +26,13 @@ class PressApp extends WebApp {
         $this->add(Service\ImageService::class);
         $this->add(Service\ImageRepository::class);
         $this->add(Controller\HomeController::class);
-    }
 
-    public function init() {
         $annotations = $this->get(AnnotationProcessor::class);
         $annotations->addNamespace('Dynart\\Press\\Controller');
         $annotations->add(RouteAnnotation::class);
+    }
 
+    public function init() {
         parent::init();
 
         $translation = $this->get(Translation::class);
